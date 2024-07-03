d5cd0db1b126bc12879604f33b51a159eaec84db
Chaitanya <boby2965@gmail.com>
2024-07-03 20:56:47 +0530

    changed to arrow function

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Hi!, My Name is Chaitanya.</p>
-        <p>This is feature 1</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <p>Hi!, My Name is Chaitanya.</p>
+//         <p>This is feature 1</p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+const App= () => (
+  <h1>Hello ! Welcome to Feature 1</h1>
+)
 
 export default App;
