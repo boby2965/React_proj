55e3e34b2fed57293f79c90aefca343c376d52fb
Chaitanya <boby2965@gmail.com>
2024-07-17 14:26:32 +0530

    tried adding states to the class

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -5,6 +5,17 @@ import "./cards.css";
 
 const Cards = (prop) => {
   const { name, source, key, wins } = prop;
+
+  function Add() {
+    let val = querySelector("progress-bar").value;
+    val = val + 1;
+  }
+
+  function Sub() {
+    let val = querySelector("progress-bar").value;
+    val = val - 1;
+  }
+
   return (
     <div className="card">
       <CardTitle className="team-title" name={name}></CardTitle>
@@ -18,7 +29,21 @@ const Cards = (prop) => {
         <p>Trophies Won: {wins}</p>
         <p>Players: 25</p>
       </div>
-      <Button className="btn" text="Learn More"></Button>
+      <progress className="progress-bar" value={wins} max="30"></progress>
+      <div className="button-group">
+        <button className="card-button" onClick={Add} id="increase-button">
+          +
+        </button>
+        <button className="card-button" onClick={Sub} id="decrease-button">
+          -
+        </button>
+        <button className="card-button" onClick={Reset} id="reset-button">
+          Reset
+        </button>
+      </div>
+      <div className="btn">
+        <Button text="Learn More"></Button>
+      </div>
     </div>
   );
 };
